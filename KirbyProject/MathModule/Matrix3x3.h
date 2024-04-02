486e9dd045d0ae8e1046e08182cb327e5f07e4e8
ManInJungtham <rhe9788@gmail.com>
2024-04-02 20:20:33 +0900

    Fix Matrix3x3::Invert.

--- KirbyProject/MathModule/Matrix3x3.h
+++ KirbyProject/MathModule/Matrix3x3.h
@@ -56,48 +56,48 @@
 				I에 그대로 취해줄 시 X (A의 역행렬) 구할 수 있다.
 			 --------------------------------------------------------------------*/
 
-			/*-------------------------------------------------------------------
-			    LU 분해를 사용하여 행렬을 상삼각, 하삼각 행렬의 곱으로 만들어주고
-			    determinant를 구하여 행렬식을 구할 수 있다.
-				A = LU -> A = | a b c || g 0 0 |
-							  | 0 d e || h i 0 |
-							  | 0 0 f || j k l |
-			 --------------------------------------------------------------------*/
-			// -> 대상 행렬을 상삼각 행렬로 만든 행렬이 L
-			//	  단위 행렬 I에 상삼각 행렬로 만들어 줄 때 사용된 연산의 역을 더해준 값 = U.
-			// 추가: pLU 분해를 고려하지 않아도 되는 이유 => 어떤 GameObject의 World matrix (transform) = SRT
-			// S는 Scale 행렬로서 3차원 공간에서 부피가 있는 오브젝트의 경우 대각성분이 0이 될 수 없다.
-			
+			 /*-------------------------------------------------------------------
+				 LU 분해를 사용하여 행렬을 상삼각, 하삼각 행렬의 곱으로 만들어주고
+				 determinant를 구하여 행렬식을 구할 수 있다.
+				 A = LU -> A = | a b c || g 0 0 |
+							   | 0 d e || h i 0 |
+							   | 0 0 f || j k l |
+			  --------------------------------------------------------------------*/
+			  // -> 대상 행렬을 상삼각 행렬로 만든 행렬이 L
+			  //	  단위 행렬 I에 상삼각 행렬로 만들어 줄 때 사용된 연산의 역을 더해준 값 = U.
+			  // 추가: pLU 분해를 고려하지 않아도 되는 이유 => 어떤 GameObject의 World matrix (transform) = SRT
+			  // S는 Scale 행렬로서 3차원 공간에서 부피가 있는 오브젝트의 경우 대각성분이 0이 될 수 없다.
+
 			Matrix3x3 L(Raws[0], Raws[1], Raws[2]);
 			Matrix3x3 U; // AX = I 에서 X가 될 행렬, (초기 값 = I)
 			U.SetIdentity();
 
 			// 1열에 대한 기본 행 연산 -> Raw[0][0] (그대로) Raw[1][0] = 0 Raw[2][0] = 0 의 형태가 된다.
 			float record = Raws[1][0] / Raws[0][0];
-			L.Raws[1] += Raws[0] * (-1) * record;
-			U.Raws[1][0] += record; // 기록된 연산 취해준다.
+			L.Raws[1] -= Raws[0] * record;
+			U.Raws[1][0] -= record; // 기록된 연산 취해준다.
 
 			record = Raws[2][0] / Raws[0][0];
-			L.Raws[2] += Raws[0] * (-1) * record;
-			U.Raws[2][0] += record;
+			L.Raws[2] -= Raws[0] * record;
+			U.Raws[2][0] -= record;
 
 			// 2열에 대한 기본 행 연산 -> Raw[0][1] Raw[1][1] Raw[2][1] = 0의 형태가 된다.
 			record = Raws[2][1] / Raws[1][1];
-			L.Raws[2] += Raws[1] * (-1) * record;
-			U.Raws[2][1] += record;
+			L.Raws[2] -= Raws[1] * record;
+			U.Raws[2][1] -= record;
 
 			// 1행, 2행의 3열을 0으로 만들어준다.
 			record = Raws[0][2] / Raws[2][2];
-			L.Raws[0] += Raws[2] * (-1) * record;
-			U.Raws[0][2] += record;
+			L.Raws[0] -= Raws[2] * record;
+			U.Raws[0][2] -= record;
 			record = Raws[1][2] / Raws[2][2];
-			L.Raws[1] += Raws[2] * (-1) * record;
-			U.Raws[1][2] += record;
+			L.Raws[1] -= Raws[2] * record;
+			U.Raws[1][2] -= record;
 
 			// 1행 2열을 0으로 만들어준다
 			record = Raws[0][1] / Raws[1][1];
-			L.Raws[0] += Raws[1] * (-1) * record;
-			U.Raws[0][1] += record;
+			L.Raws[0] -= Raws[1] * record;
+			U.Raws[0][1] -= record;
 
 			// 대각 성분을 모두 1로 만들어 준다.
 			float inverseOp = 1 / L.Raws[0][0];
@@ -111,33 +111,6 @@
 			U.Raws[2] *= inverseOp;
 
 			return U;
-
-			//// I에 기록되었던 연산들을 차례대로 더해주면 U 완성
-			//U.Raws[1][0] += record_1_0;
-			//U.Raws[2][0] += record_2_0;
-			//U.Raws[2][1] += record_2_1;
-
-			//// det(AB) = det(A) * det(B)
-			//// 상삼각, 하삼각행렬의 행렬식 = 대각 성분의 곱
-			//float d = L.Raws[0][0] * L.Raws[1][1] * L.Raws[2][2] * U.Raws[0][0] * U.Raws[1][1] * U.Raws[2][2];
-
-			/*float d = Raws[0][0] * (Raws[1][1] * Raws[2][2] - Raws[2][1] * Raws[1][2]) - Raws[0][1] * (Raws[1][0] * Raws[2][2] - Raws[1][2] * Raws[2][0]) + Raws[0][2] * (Raws[1][0] * Raws[2][1] - Raws[1][1] * Raws[2][0]);
-			if (d != 0.)
-			{
-				Matrix3x3 invertedMatrix;
-				float id = 1 / d;
-				invertedMatrix.Raws[0][0] = (Raws[1][1] * Raws[2][2] - Raws[2][1] * Raws[1][2]) * id;
-				invertedMatrix.Raws[0][1] = (Raws[0][2] * Raws[2][1] - Raws[0][1] * Raws[2][2]) * id;
-				invertedMatrix.Raws[0][2] = (Raws[0][1] * Raws[1][2] - Raws[0][2] * Raws[1][1]) * id;
-				invertedMatrix.Raws[1][0] = (Raws[1][2] * Raws[2][0] - Raws[1][0] * Raws[2][2]) * id;
-				invertedMatrix.Raws[1][1] = (Raws[0][0] * Raws[2][2] - Raws[0][2] * Raws[2][0]) * id;
-				invertedMatrix.Raws[1][2] = (Raws[1][0] * Raws[0][2] - Raws[0][0] * Raws[1][2]) * id;
-				invertedMatrix.Raws[2][0] = (Raws[1][0] * Raws[2][1] - Raws[2][0] * Raws[1][1]) * id;
-				invertedMatrix.Raws[2][1] = (Raws[2][0] * Raws[0][1] - Raws[0][0] * Raws[2][1]) * id;
-				invertedMatrix.Raws[2][2] = (Raws[0][0] * Raws[1][1] - Raws[1][0] * Raws[0][1]) * id;
-
-				return invertedMatrix;
-			}*/
 		}
 
 		FORCEINLINE Vector2 Translation() const noexcept;
